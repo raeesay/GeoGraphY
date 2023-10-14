3f9515da140c4da536b927ff176c17b222d7eefd
abigailhayes <abigail.joy.hayes@gmail.com>
2023-10-14 19:49:20 +0200

    Started question specific dbpedia functions. Still need to add it handling for no leaders listed, and to filter the results to just uris.

--- src/utils/dbpedia_utils.py
+++ src/utils/dbpedia_utils.py
@@ -1,5 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
+# For sending requests to dbpedia ------------------------------------------------------------------
+
 def dbp_select_o(s,p):
     """
     - Function to form the select query
@@ -26,6 +28,8 @@ def dbp_extract_o(s,p):
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     return(results)
+
+# For handling the returned data from dbpedia -----------------------------------------------------
 
 def dbp_json_to_list(json):
     """
@@ -57,4 +61,28 @@ def dbp_list_uri_to_text(string_list):
     - Returns a list of strings
     """
     return([dbp_uri_to_text(string) for string in string_list])
+
+# Question specific functions --------------------------------------------------------------------
+
+def dbp_countryCode(country_uri):
+    """
+    dbp_countryCode
+    - Function to retrieve the dialling code for the country provided as input
+    - The input can either be the URI or also dbp:Country
+    - Returns a string with the dialling code
+    """
+    working = dbp_extract_o(s=country_uri, p="dbo:countryCode")
+    return(dbp_json_to_list(working)[0])
+
+def dbp_leaderName(country_uri):
+    """
+    dbp_leaderName
+    - Function to retrieve the leaders' names for the country provided as input
+    - The input can either be the URI or also dbp:Country
+    - Returns a string with the leader names, with / between where there are multiple
+    """
+    working = dbp_extract_o(s=country_uri, p="dbp:leaderName")
+    working = dbp_json_to_list(working)
+    working = dbp_list_uri_to_text(working)
+    return("/".join(working))
 
