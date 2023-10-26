c00350065529877e5e363b415aa58bdab445f838
Jana Jung <jana@dr-a-jung.de>
2023-10-26 16:00:57 +0200

    Wrong answer generation for currency questions

--- src/utils/wrong_answers.py
+++ src/utils/wrong_answers.py
@@ -61,3 +61,34 @@ def wrong_answers_capital(rdf_countries, right_answer):
         wrong_answers.append(row.capital_uri)
 
     return wrong_answers
+
+def wrong_answers_currency(rdf_countries, right_answer):
+    """
+    - Function to retrieve wrong answers for a question with currency as answers
+    - Inputs
+        - countries rdf file
+        - right_answer to the corresponding question in form of a currency URI
+    - Returns a list of 3 different currency URIs of 3 randomly chosen countries
+    """
+
+    # Query 3 random (distinct) currencies
+    query_currency = f"""
+            PREFIX gn: <http://www.geonames.org/ontology#>
+            PREFIX geographis: <http://telegraphis.net/ontology/geography/geography#>
+
+            SELECT DISTINCT ?currency_uri
+            WHERE {{
+                ?country_uri a gn:Country;
+                            geographis:currency ?currency_uri .
+                FILTER(?currency_uri != <{right_answer}>)
+            }}
+            ORDER BY RAND()
+            LIMIT 3
+        """
+
+    wrong_answers = []
+
+    for row in rdf_countries.query(query_currency):
+        wrong_answers.append(row.currency_uri)
+
+    return wrong_answers
