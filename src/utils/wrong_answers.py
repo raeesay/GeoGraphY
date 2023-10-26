b1d6beefee5a9b0628f0e18e1b7ee7773550fbb5
Jana Jung <jana@dr-a-jung.de>
2023-10-26 15:27:41 +0200

    Wrong answer generation for capitals

--- src/utils/wrong_answers.py
+++ src/utils/wrong_answers.py
@@ -9,11 +9,8 @@ def wrong_answers_country(rdf_countries, right_answer):
         - STILL MISSING: optional continent URI!!!
     - Returns a list of 3 country URIs
     """
-    # Generate a random offset
-    max_offset = 240
-    random_offset = random.randint(0, max_offset)
 
-    # Query a random country
+    # Query 3 random countries
     query_country = f"""
         PREFIX gn: <http://www.geonames.org/ontology#>
 
@@ -34,3 +31,33 @@ def wrong_answers_country(rdf_countries, right_answer):
 
     return wrong_answers
 
+def wrong_answers_capital(rdf_countries, right_answer):
+    """
+        - Function to retrieve wrong answers for a question with countries as answers
+        - Inputs
+            - countries rdf file
+            - right_answer to the corresponding question in form of a captial URI
+        - Returns a list of 3 captial URIs of 3 randomly chosen countries
+        """
+
+    # Query 3 random capitals
+    query_capital = f"""
+            PREFIX gn: <http://www.geonames.org/ontology#>
+            PREFIX geographis: <http://telegraphis.net/ontology/geography/geography#>
+
+            SELECT ?capital_uri
+            WHERE {{
+                ?country_uri a gn:Country;
+                            geographis:capital ?capital_uri .
+                FILTER(?capital_uri != <{right_answer}>)
+            }}
+            ORDER BY RAND()
+            LIMIT 3
+        """
+
+    wrong_answers = []
+
+    for row in rdf_countries.query(query_capital):
+        wrong_answers.append(row.capital_uri)
+
+    return wrong_answers
