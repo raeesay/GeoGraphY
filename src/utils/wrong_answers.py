360aa504c069589fb8dc40790658ef2b8ad5709d
Jana Jung <jana@dr-a-jung.de>
2023-10-27 14:54:13 +0200

    Wrong answer generation for country questions that depend on continent

--- src/utils/wrong_answers.py
+++ src/utils/wrong_answers.py
@@ -6,7 +6,6 @@ def wrong_answers_country(rdf_countries, right_answer):
     - Inputs
         - countries rdf file
         - right_answer to the corresponding question in form of a country URI
-        - STILL MISSING: optional continent URI!!!
     - Returns a list of 3 country URIs
     """
 
@@ -19,6 +18,37 @@ def wrong_answers_country(rdf_countries, right_answer):
             ?country_uri a gn:Country;
                         geographis:onContinent ?continent_uri
             FILTER(?country_uri != <{right_answer}>)
+        }}
+        ORDER BY RAND()
+        LIMIT 3
+    """
+
+    wrong_answers = []
+
+    for row in rdf_countries.query(query_country):
+        wrong_answers.append(row.country_uri)
+
+    return wrong_answers
+
+def wrong_answers_country_continent(rdf_countries, right_answer, continent_uri):
+    """
+    - Function to retrieve wrong answers for a question with countries as answers that depend on continent
+    - Inputs
+        - countries rdf file
+        - right_answer to the corresponding question in form of a country URI
+        - continent URI
+    - Returns a list of 3 country URIs
+    """
+
+    # Query 3 random countries
+    query_country = f"""
+        PREFIX gn: <http://www.geonames.org/ontology#>
+
+        SELECT ?country_uri
+        WHERE {{
+            ?country_uri a gn:Country;
+                        geographis:onContinent ?continent_uri
+            FILTER(?country_uri != <{right_answer}> && ?continent_uri = <{continent_uri}>)
         }}
         ORDER BY RAND()
         LIMIT 3
