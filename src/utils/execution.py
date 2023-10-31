2624e6ac9812c24b32bde5467342c5d4082f859e
PatrickHeuer <patrick-heuer@gmx.de>
2023-10-31 21:07:25 +0100

    fix and expand the settings method

--- src/utils/execution.py
+++ src/utils/execution.py
@@ -57,16 +57,28 @@ class Quiz:
 
 
     def getSettings(self):
-        print("Available Difficulty-Levels for this Quiz:")
+        print("Available difficulty-levels for this quiz:")
         print("1: Easy")
         print("2: Medium")
         print("3: Advanced")
-        difficulty = input("How difficult shall the questions be?")
+        difficulty = input("How difficult shall the questions be? Please choose a number from 1 to 3: ")
         if difficulty not in ["1", "2", "3"]:
             print("Wrong input for 'difficulty'. The quiz will have the difficulty 'Easy' as default value.")
             difficulty = 1
+        elif difficulty == "1":
+            print("The quiz will have the difficulty 'Easy'.")
+        elif difficulty == "2":
+            print("The quiz will have the difficulty 'Medium'.")
+        else:
+            print("The quiz will have the difficulty 'Advanced'.")
 
-        nQuestions = input("Please choose the number of questions you want to be asked. The number shall be between 1 and 25.")
+        nQuestions = input("Please choose the number of questions you want to be asked. Please choose a number from 1 to 25: ")
         if int(nQuestions) < 1 or int(nQuestions) > 25:
-            print("Wrong input for the number of Questions. The quiz will have 10 questions as default value.")
+            print("Wrong input for the number of questions. The quiz will have 10 questions as default value.")
             nQuestions = 10
+        else:
+            print(f"Perfect! You will be asked {nQuestions} in the following quiz.")
+
+        print("Good luck! :)")
+
+        return difficulty, nQuestions
